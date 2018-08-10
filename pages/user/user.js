e8a8cd5eb7300a1adaed241246d76c1f8ab2ba5d
杨晓宗 <yangxz@calis.edu.cn>
2018-08-10 14:05:44 +0800

    userinfo

--- pages/user/user.js
+++ pages/user/user.js
@@ -9,19 +9,14 @@ Page({
     logged: false,
     display: false,
     takeSession: false,
-    today: util.formatDate(new Date()),
-    username: '',
-    requestResult: '',
-    switch1: true
+    iconSize: 20
   },
-  onChange(event) {
-    const detail = event.detail;
-    this.setData({
-      'switch1': detail.value
+  info: function () {
+    wx.navigateTo({
+      url: '/pages/user-info/user-info'
     })
-
   },
-  // 用户登录示例
+  // 用户登录
   login: function () {
 
     var that = this;
@@ -41,88 +36,5 @@ Page({
         console.log('登录失败', err);
       }
     });
-  },
-  clearInput: function (e) {
-    switch (e.currentTarget.id) {
-      case 'clear-username':
-        this.setData({
-          username: ''
-        });
-        break;
-      case 'clear-bookname':
-        this.setData({
-          bookname: ''
-        });
-        break;
-      case 'clear-readsecond':
-        this.setData({
-          readsecond: ''
-        });
-        break;
-    }
-  },
-  bindUsernameInput: function (e) {
-    this.setData({
-      username: e.detail.value
-    });
-  },
-  bindBooknameInput: function (e) {
-    this.setData({
-      bookname: e.detail.value
-    });
-  },
-  bindReadsecondInput: function (e) {
-    this.setData({
-      readsecond: e.detail.value
-    });
-  },
-  sure: function () {
-    // 确认按钮
-    var that = this;
-    if (that.data.username.length < 2) {
-      wx.showModal({
-        title: '提示信息',
-        content: '宝儿的名子不得少于2位',
-        showCancel: false
-      });
-      return false;
-    }
-
-    //修改呢称
-    console.log('r');
-    if (that.data.userInfo) {
-      that.data.userInfo.nickName = that.data.username;
-    }
-
-    qcloud.request({
-      url: `${config.service.host}/weapp/userinfo`,
-      method: 'PUT',
-      data: that.data.userInfo,
-      success(result) {
-        that.setData({
-          display: false,
-          userInfo: result.data.data
-        });
-
-        let session = qcloud.getSession();
-        session.userInfo.nickName = that.data.userInfo.nickName;
-        qcloud.setSession(session);
-
-
-      },
-      fail(error) {
-        util.showModel('请求失败', error);
-      }
-    })
-  },
-  choose: function () {
-    //切换显示功能
-    var that = this;
-    that.setData({
-      display: !that.data.display
-    });
-  },
-  sureRead: function () {
-    // 确认按钮
   }
 })
