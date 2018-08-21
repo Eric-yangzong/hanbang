5250c42a373bdfa156d0b72c0ed952e02fd97f6e
杨晓宗 <yangzong_9958@126.com>
2018-08-21 21:15:58 +0800

    用户信息修改

--- pages/user-info/user-info.js
+++ pages/user-info/user-info.js
@@ -10,24 +10,38 @@ Page({
     display: false,
     takeSession: false,
     today: util.formatDate(new Date()),
-    username: '',
-    requestResult: '',
-    switch1: true,
+    username: '',//姓名
+    phone: '',//电话
+    sex: '1',//性别
+    company: '',//所在单位
+    card: '',//身份证号
+    school: '',//所报学校
+    professional: '',//所报专业
+    type: '1',//形式
+    weichat: '',//微信号
+    remark: '',//备注
     iconSize: 20,
-    value1: '',
-    value2: '',
-    value3: '',
-    value4: '输入框已禁用',
-    value5: '',
-    value6: '',
-    value7: ''
-  },
-  onChange(event) {
-    const detail = event.detail;
-    this.setData({
-      'switch1': detail.value
-    })
-
+    sexRadio: [{
+      id: 1,
+      name: '男',
+      checked: true
+    }, {
+      id: 2,
+      name: '女'
+    }],
+    btype: [{
+      id: 1,
+      name: '函授',
+      checked: true
+    }, {
+      id: 2,
+      name: '业余'
+    }, {
+      id: 3,
+      name: '脱产'
+    }],
+    current: '男',
+    position: 'left'
   },
   // 用户登录示例
   login: function () {
@@ -49,47 +63,81 @@ Page({
         console.log('登录失败', err);
       }
     });
-  },
-  clearInput: function (e) {
-    switch (e.currentTarget.id) {
-      case 'clear-username':
-        this.setData({
-          username: ''
-        });
-        break;
-      case 'clear-bookname':
-        this.setData({
-          bookname: ''
-        });
-        break;
-      case 'clear-readsecond':
-        this.setData({
-          readsecond: ''
-        });
-        break;
-    }
-  },
+  },//姓名
   bindUsernameInput: function (e) {
     this.setData({
-      username: e.detail.value
+      username: e.detail.detail.value
     });
-  },
-  bindBooknameInput: function (e) {
+  },//电话
+  bindPhoneInput: function (e) {
     this.setData({
-      bookname: e.detail.value
+      phone: e.detail.detail.value
     });
-  },
-  bindReadsecondInput: function (e) {
+  },//性别
+  sexChange: function (e) {
     this.setData({
-      readsecond: e.detail.value
+      sex: e.detail.value
     });
+
+  },//所在单位
+  bindCompanyInput: function (e) {
+    this.setData({
+      company: e.detail.detail.value
+    });
+  },//身份证号
+  bindCardInput: function (e) {
+    this.setData({
+      card: e.detail.detail.value
+    });
+  },//所报学校
+  bindSchoolInput: function (e) {
+    this.setData({
+      school: e.detail.detail.value
+    });
+  },//所报专业
+  bindProfessionalInput: function (e) {
+    this.setData({
+      professional: e.detail.detail.value
+    });
+  },//形式
+  typeChange: function (e) {
+    this.setData({
+      type: e.detail.value
+    });
+  },//微信
+  bindWeichatInput: function (e) {
+    this.setData({
+      weichat: e.detail.detail.value
+    });
+  },//备注
+  bindRemarkInput: function (e) {
+    this.setData({
+      remark: e.detail.detail.value
+    });
+    console.log(this.data);
   },
   handleClick: function () {
     // 确认按钮
     var that = this;
 
+    if(!that.data.userInfo.nickName){
+      let session = qcloud.getSession();
+      that.data.userInfo = session.userInfo;
+    }
+
+
+
+
     if (that.data.userInfo) {
-      that.data.userInfo.nickName = that.data.username;
+      that.data.userInfo.nickName = that.data.username;//用户名
+      that.data.userInfo["sex"] = that.data.sex;//性别
+      that.data.userInfo["company"] = that.data.company;//所在单位
+      that.data.userInfo["card"] = that.data.card;//身份证号
+      that.data.userInfo["school"] = that.data.school;//所报学校
+      that.data.userInfo["professional"] = that.data.professional;//所报专业
+      that.data.userInfo["type"] = that.data.type;//形式
+      that.data.userInfo["weichat"] = that.data.weichat;//微信号
+      that.data.userInfo["remark"] = that.data.remark;//备注 
     }
 
     qcloud.request({
@@ -112,15 +160,5 @@ Page({
         util.showModel('请求失败', error);
       }
     })
-  },
-  choose: function () {
-    //切换显示功能
-    var that = this;
-    that.setData({
-      display: !that.data.display
-    });
-  },
-  sureRead: function () {
-    // 确认按钮
-  }//事件处理函数
+  }
 })
